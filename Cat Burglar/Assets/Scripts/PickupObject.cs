52600632ad1480ac2d37b43f1cf3dc08bd89bfab
Steven <43658696+Br1ckmaster@users.noreply.github.com>
2018-10-11 15:08:10 +1100

    Character Controller & AI
    Changed name of project. added a simple character controller, AI, and also objects you can pickup

---
+++ Cat Burglar/Assets/Scripts/PickupObject.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PickupObject : MonoBehaviour {
+
+    public Text scoreText;
+
+    private bool canPickup;
+    private int score;
+
+    void Start()
+    {
+        score = 0;
+        ScoreText();
+    }
+	
+	void Update()
+    {
+        if (canPickup && Input.GetKeyDown(KeyCode.E)) //Player pressing E to pickup object
+        {
+            ObjectPickup();
+            Score();
+            ScoreText();
+        }
+    
+    }
+
+    void OnTriggerEnter(Collider collider) //When player collides with object
+    {
+      if(collider.gameObject.name == "Player")
+        {
+            Debug.Log("Entered Collision");
+            canPickup = true;
+        }
+    }
+
+    void OnTriggerExit(Collider collider) //When player exits object collider
+    {
+        if (collider.gameObject.name == "Player")
+        {
+            Debug.Log("Exited Collision");
+            canPickup = false;
+        }
+    }
+
+    void ObjectPickup() //Pickup object
+    {
+        Destroy(this.gameObject);
+    }
+
+    void OnGUI() //Displays tooltip
+    {
+        if(canPickup)
+        {
+            GUI.Box(new Rect(0, 60, 300, 50), "Press E to pickup");
+        }
+    }
+
+    void ScoreText() //Displays score text
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+   
+    void Score() //Adding score
+    {
+        score = score + 1;
+    }
+}
---
+++ Cat Burglar/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    void Update() //Player Movement
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+    }
+
+}
