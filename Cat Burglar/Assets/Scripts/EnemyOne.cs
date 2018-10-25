4c20c8af6663efd6e10a6d5361759312f12452ce
Steven <43658696+Br1ckmaster@users.noreply.github.com>
2018-10-25 15:12:50 +1100

    EnemyOne script fixed
    Animations should now fully work when targetting both players

--- Cat Burglar/Assets/Scripts/EnemyOne.cs
+++ Cat Burglar/Assets/Scripts/EnemyOne.cs
@@ -6,7 +6,7 @@ public class EnemyOne: MonoBehaviour
 {
 
     public Transform playerOne;
-    //public Transform playerTwo;
+    public Transform playerTwo;
     public double enemyRadius = 2;
     public float enemySpeed;
 	static Animator anim;
@@ -18,9 +18,9 @@ public class EnemyOne: MonoBehaviour
     void Update()
     {
         Vector3 directionOne = playerOne.position - this.transform.position;
-        //Vector3 directionTwo = playerTwo.position - this.transform.position;
+        Vector3 directionTwo = playerTwo.position - this.transform.position;
         //float angle = Vector3.Angle(direction, this.transform.forward);
-		if (Vector3.Distance (playerOne.position, this.transform.position) < enemyRadius /*&& angle < 30*/) 
+        if (Vector3.Distance (playerOne.position, this.transform.position) < enemyRadius /*&& angle < 30*/) 
 		{
 			directionOne.y = 0;
 			this.transform.rotation = Quaternion.Slerp (this.transform.rotation, Quaternion.LookRotation (directionOne), 0.1f);
@@ -38,24 +38,31 @@ public class EnemyOne: MonoBehaviour
 			}
 
 		} 
-		else 
-		{
-			anim.SetBool ("IsIdle", true);
-			anim.SetBool ("IsWalking", false);
-			anim.SetBool ("IsAttacking", false);
-		}
 
-       // else if (Vector3.Distance(playerTwo.position, this.transform.position) < enemyRadius /*&& angle < 30*/)
-       /* {
+
+        else if (Vector3.Distance(playerTwo.position, this.transform.position) < enemyRadius /*&& angle < 30*/)
+        {
             directionTwo.y = 0;
             this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(directionTwo), 0.1f);
-			anim.SetBool ("isIdle", false);
+            anim.SetBool("IsIdle", false);
             if (directionTwo.magnitude > 1)
             {
                 this.transform.Translate(0, 0, enemySpeed);
-				anim.SetBool ("isWalking", true);
+                anim.SetBool("IsWalking", true);
+                anim.SetBool("IsAttacking", false);
             }
+            else
+            {
+                anim.SetBool("IsAttacking", true);
+                anim.SetBool("IsWalking", false);
+            }
+        }
 
-        }*/
+        else
+        {
+            anim.SetBool("IsIdle", true);
+            anim.SetBool("IsWalking", false);
+            anim.SetBool("IsAttacking", false);
+        }
     }
 }
