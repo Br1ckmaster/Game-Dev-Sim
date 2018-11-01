d521041c9e720f9ba77e8427415f6f6125f5672e
undoburn <43664608+undoburn@users.noreply.github.com>
2018-11-02 10:34:06 +1100

    enemy and player animations

--- Cat Burglar/Assets/Scripts/EnemyTwo.cs
+++ Cat Burglar/Assets/Scripts/EnemyTwo.cs
@@ -7,35 +7,63 @@ public class EnemyTwo : MonoBehaviour
 
     public Transform playerOne;
     public Transform playerTwo;
-    public double enemyRadius = 5;
+	public double enemyRadiusP1 = 2;
+	public double enemyRadiusP2 = 2;
+	public float enemySpeed;
+	static Animator anim;
 
-    public float enemySpeed;
+	void Start ()
+	{
+		anim = GetComponent<Animator>();
+	}
     void Update()
     {
         Vector3 directionOne = playerOne.position - this.transform.position;
         Vector3 directionTwo = playerTwo.position - this.transform.position;
         //float angle = Vector3.Angle(direction, this.transform.forward);
-        if (Vector3.Distance(playerTwo.position, this.transform.position) < enemyRadius /*&& angle < 30*/)
-        {
-            directionTwo.y = 0;
-            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(directionTwo), 0.1f);
-            if (directionTwo.magnitude > 1)
-            {
-                this.transform.Translate(0, 0, enemySpeed);
-            }
+		if (Vector3.Distance (playerOne.position, this.transform.position) < enemyRadiusP1 /*&& angle < 30*/) 
+		{
+			directionOne.y = 0;
+			this.transform.rotation = Quaternion.Slerp (this.transform.rotation, Quaternion.LookRotation (directionOne), 0.1f);
+			anim.SetBool ("IsIdle", false);
+			if (directionOne.magnitude > 1) 
+			{
+				this.transform.Translate (0, 0, enemySpeed);
+				anim.SetBool ("IsWalking", true);
+				anim.SetBool ("IsAttacking", false);
+			} 
+			else 
+			{
+				anim.SetBool ("IsAttacking", true);
+				anim.SetBool ("IsWalking", false);
+			}
 
-        }
+		} 
 
-        else if (Vector3.Distance(playerOne.position, this.transform.position) < enemyRadius /*&& angle < 30*/)
-        {
-            directionOne.y = 0;
-            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(directionOne), 0.1f);
-            if (directionOne.magnitude > 1)
-            {
-                this.transform.Translate(0, 0, enemySpeed);
-            }
 
-        }
-        
-    }
+		else if (Vector3.Distance(playerTwo.position, this.transform.position) < enemyRadiusP2 /*&& angle < 30*/)
+		{
+			directionTwo.y = 0;
+			this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(directionTwo), 0.1f);
+			anim.SetBool("IsIdle", false);
+			if (directionTwo.magnitude > 1)
+			{
+				this.transform.Translate(0, 0, enemySpeed);
+				anim.SetBool("IsWalking", true);
+				anim.SetBool("IsAttacking", false);
+			}
+			else
+			{
+				anim.SetBool("IsAttacking", true);
+				anim.SetBool("IsWalking", false);
+			}
+		}
+
+		else
+		{
+			anim.SetBool("IsIdle", true);
+			anim.SetBool("IsWalking", false);
+			anim.SetBool("IsAttacking", false);
+		}
+	}
 }
--- Cat Burglar/Assets/Scripts/PlayerTwoController.cs
+++ Cat Burglar/Assets/Scripts/PlayerTwoController.cs
@@ -7,13 +7,15 @@ public class PlayerTwoController : MonoBehaviour
     [SerializeField] private float movementSpeed;
 
     CharacterController playerTwo;
-    void Start()
+	Animator animControl;
+	private void Start()
     {
         playerTwo = GetComponent<CharacterController>();
+		animControl = GameObject.FindGameObjectWithTag("PlayerTwo").GetComponent<Animator>();
     }
     void Update() //Player Movement
     {
-
+		PlayerAnimator();
         Vector2 input = new Vector2(Input.GetAxisRaw("P2_Horizontal"), Input.GetAxisRaw("P2_Vertical"));
         Vector2 inputDir = input.normalized;
 
@@ -27,5 +29,29 @@ public class PlayerTwoController : MonoBehaviour
         Vector3 velocity = transform.forward * speed;
         playerTwo.Move(velocity * Time.deltaTime);
     }
+
+	void PlayerAnimator()
+	{
+
+		if(Input.GetButton("P2_Horizontal") || (Input.GetButton("P2_Vertical")))
+		{
+			animControl.SetBool("IsWalking", true);
+			animControl.SetBool("IsIdle", false);
+			animControl.SetBool("IsPickup", false);
+		}
+		else if(Input.GetButton("P2_Pickup"))
+		{
+			animControl.SetBool("IsWalking", false);
+			animControl.SetBool("IsIdle", false);
+			animControl.SetBool("IsPickup", true);
+		}
+
+		else
+		{
+			animControl.SetBool("IsWalking", false);
+			animControl.SetBool("IsIdle", true);
+			animControl.SetBool("IsPickup", false);
+		}
+	}
 
 }
