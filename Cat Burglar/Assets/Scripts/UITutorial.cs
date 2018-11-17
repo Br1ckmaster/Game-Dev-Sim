7d927352f26b106348d8c0d020697d016c5501c1
Ryan Dempsey <undoburn@gmail.com>
2018-11-17 13:54:18 +1100

    Player fixes
    removed an audio player from each player as they each had 2, saved new prefab

---
+++ Cat Burglar/Assets/Scripts/UITutorial.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITutorial : MonoBehaviour 
+{
+	[SerializeField] private Image customImage1;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag ("Player")) 
+		{
+			customImage1.enabled = true;
+		}
+	}
+
+	void OnTriggerExir(Collider other)
+	{
+		if (other.CompareTag ("Player")) 
+		{
+			customImage1.enabled = false;
+		}
+	}
+}
