aeafc52936a21006d67a21e783a2daa3c6630381
Ryan Dempsey <undoburn@gmail.com>
2018-11-21 22:29:01 +1100

    UI time pin

---
+++ Cat Burglar/Assets/Scripts/UITimer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITimer : MonoBehaviour {
+
+	Image FillImg;
+	float timeAmt = 10;
+	float time;
+	//public Text timeText;
+
+	// Use this for initialization
+	void Start () 
+	{
+		FillImg = this.GetComponent<Image> ();
+		time = timeAmt;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (time > 0) 
+		{
+			time -= Time.deltaTime;
+			FillImg.fillAmount = time / timeAmt;
+			//timeText.text = "Time : " + time.ToString ("F");
+		}
+	}
+}
