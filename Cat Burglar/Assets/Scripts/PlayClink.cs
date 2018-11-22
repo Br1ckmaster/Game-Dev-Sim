0fdd559f6f2e00ca0e08d38a9c5014770f5661ea
undoburn <43664608+undoburn@users.noreply.github.com>
2018-11-22 13:33:40 +1100

    added clinksfx script

---
+++ Cat Burglar/Assets/Scripts/PlayClink.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayClink : MonoBehaviour {
+
+    public bool inTrigger;
+    public AudioClip clinkSound;
+    private AudioSource source;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        inTrigger = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        inTrigger = false;
+    }
+
+    void Awake()
+    {
+
+        source = GetComponent<AudioSource>();
+
+    }
+
+    void Update()
+    {
+        if (inTrigger)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftControl))
+            {
+                source.PlayOneShot(clinkSound);
+               // DoorScript.doorKey = true;
+                StartCoroutine(Key());
+            }
+        }
+    }
+
+    IEnumerator Key()
+    {
+        yield return new WaitForSeconds(0.5f);
+      //  Destroy(this.gameObject);
+    }
+}
