25356c37df326a699dfd6850076d26d6e02c2c15
Steven <43658696+Br1ckmaster@users.noreply.github.com>
2018-11-22 16:52:08 +1100

    Cleaned up code

--- Cat Burglar/Assets/Scripts/UITimer.cs
+++ Cat Burglar/Assets/Scripts/UITimer.cs
@@ -3,32 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UITimer : MonoBehaviour {
+public class UITimer : MonoBehaviour
+{
 
+    ////Image FillImg;
+    //float timeAmt = 10;
+    //float time;
 
-    
-	//Image FillImg;
-	float timeAmt = 10;
-	float time;
+    public NightCycle nightCycle;
+
+    private float x = 0;
+    private float y = 0;
+    private float z = 90;
 	//public Text timeText;
 
-	// Use this for initialization
 	void Start () 
 	{
         RectTransform rectTransform = GetComponent<RectTransform>();
-        rectTransform.Rotate(new Vector3(0, 0, 90));
+        rectTransform.Rotate(new Vector3(x, y, z));
         //FillImg = this.GetComponent<Image> ();
-		time = timeAmt;
+		//time = timeAmt;
 	}
 	
-	// Update is called once per frame
 	void Update () 
 	{
-		if (time > 0) 
-		{
-			time -= Time.deltaTime;
-			//FillImg.fillAmount = time / timeAmt;
-            //timeText.text = "Time : " + time.ToString ("F");
-        }
+
+		//if (time > 0) 
+		//{
+		//	time -= Time.deltaTime;
+		//	//FillImg.fillAmount = time / timeAmt;
+            
+  //          //timeText.text = "Time : " + time.ToString ("F");
+  //      }
 	}
+
+    void UpdateHand()
+    {
+
+    }
 }
