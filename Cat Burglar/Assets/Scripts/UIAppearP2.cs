03525e8399479f34672780896b63b94981640c9e
undoburn <43664608+undoburn@users.noreply.github.com>
2018-11-23 12:32:08 +1100

    P2 ui interact

---
+++ Cat Burglar/Assets/Scripts/UIAppearP2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIAppearP2 : MonoBehaviour {
+
+    [SerializeField] private Image customImage;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PlayerTwo"))
+        {
+            customImage.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("PlayerTwo"))
+        {
+            customImage.enabled = false;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (customImage != null)
+        {
+            customImage.enabled = false;
+        }
+    }
+}
